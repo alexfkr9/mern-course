84111556d2a956547cd69fe5d7db5dda5be750d5
Alexfkr9 <alexfkr9@gmail.com>
2021-09-12 18:13:34 +0300

    1

---
+++ client/build/precache-manifest.27dd6cd8a79f15322d52787157fd2d22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf87a74f76f27c128453d506bff37244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9035882c156f4fa679",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "7ef53f9af8829a0851d3",
+    "url": "/static/css/main.8732f41c.chunk.css"
+  },
+  {
+    "revision": "ee9035882c156f4fa679",
+    "url": "/static/js/2.b6b3ead5.chunk.js"
+  },
+  {
+    "revision": "14c9108fd5a38194785d545908fb6cbd",
+    "url": "/static/js/2.b6b3ead5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ef53f9af8829a0851d3",
+    "url": "/static/js/main.53829774.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
